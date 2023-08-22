24060bc5ad49bc1504bbe4870d84b95aa1ad594f
yosief14 <yosief10@outlook.com>
2023-08-22 15:11:37 -0600

    Initial commit

---
+++ kanban-client/tailwind.config.js
@@ -0,0 +1,30 @@
+import {nextui} from '@nextui-org/theme'
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui({
+    themes: {
+      light: {
+        colors: {
+          primary: "#7828c8",
+        }
+      },
+      dark: {
+        colors: {
+          primary: "#9353d3",
+          background: "#2A2A2A",
+        }
+      },
+    },
+  })],
+}
